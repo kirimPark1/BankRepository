d9d79ddb4c2a0c2358261220974894840750f02f
kirim <kirim.park@naver.com>
2023-06-01 13:46:10 +0900

    first commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "../BankKATA/Bank.cpp"
+
+class AccountFixture : public testing::Test
+{
+public:
+	Account account{ 10000 };
+};
+TEST_F(AccountFixture, CreateAccountInit10000won) {
+	EXPECT_EQ(10000, account.getBalance());
+}
+
+TEST_F(AccountFixture, Deposit) {
+	account.deposit(500);
+	EXPECT_EQ(10500, account.getBalance());
+}
+
+TEST_F(AccountFixture, Withdraw) {
+	account.withdraw(600);
+	EXPECT_EQ(9400, account.getBalance());
+}

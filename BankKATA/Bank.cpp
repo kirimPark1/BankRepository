66ead4ee64dacb8e35ab390dd62673c547904b93
kirim <kirim.park@naver.com>
2023-06-01 14:03:00 +0900

    add feature - Interest

--- BankKATA/Bank.cpp
+++ BankKATA/Bank.cpp
@@ -1,8 +1,8 @@
 class Account
 {
 public:
-	explicit Account(int money)
-		: balance(money)
+	explicit Account(int money, int percentage)
+		: balance(money), interest(percentage)
 	{
 	}
 
@@ -21,7 +21,27 @@ public:
 		balance -= money;
 	}
 
+	int getBalanceWithInterest(int year)
+	{
+		int result = balance;
+		double intersetForYear = 1 + (static_cast<double>(getInterest()) / 100.0);
+		for (int i = 0; i < year; i++)
+		{
+			result *= intersetForYear;
+		}
+		return result;
+	}
+
+	void setInterest(int Percentage)
+	{
+		interest = Percentage;
+	}
+	int getInterest()
+	{
+		return interest;
+	}
+
 private:
 	int balance;
-	
+	int interest;
 };
